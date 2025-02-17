d73c5438313ee7fdc44872fe9ffeb0148bd63d1f
captainboogey <anik35-700@diu.edu.bd>
2025-02-17 19:40:16 +0600

    single data load addded

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -34,6 +34,14 @@ async function run() {
    
     const result = await cursor.toArray()
     res.send(result);
+   })
+
+   app.get("/services/:id",async(req,res)=>{
+      const id = req.params.id;
+       const query = {_id: new ObjectId(id)}
+       const options ={
+        projection:{title:1,img:1,}
+       }
    })
 
 
